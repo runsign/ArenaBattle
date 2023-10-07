7cba981cd11e5ef58dacc487a6c751976c8a5eca
runsign <soyoja@naver.com>
2023-10-08 08:52:45 +0900

    159

--- Source/ArenaBattle/private/ABPawn.cpp
+++ Source/ArenaBattle/private/ABPawn.cpp
@@ -9,6 +9,37 @@ AABPawn::AABPawn()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CAPSULE"));
+	Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("MESH"));
+	Movement = CreateDefaultSubobject<UFloatingPawnMovement>(TEXT("MOVEMENT"));
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SPRINGARM"));
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("CAMERA"));
+
+	RootComponent = Capsule;
+	Mesh->SetupAttachment(Capsule);
+	SpringArm->SetupAttachment(Capsule);
+	Camera->SetupAttachment(SpringArm);
+
+	Capsule->SetCapsuleHalfHeight(88.0f);
+	Capsule->SetCapsuleRadius(34.0f);
+	Mesh->SetRelativeLocationAndRotation(FVector(0.f, 0.f, -88.f), FRotator(0.f, -90.f, 0.f));
+	SpringArm->TargetArmLength = 400.f;
+	SpringArm->SetRelativeRotation(FRotator(-15.f, 0.f, 0.f));
+
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> SK_CARDBOARD(TEXT("/Script/Engine.SkeletalMesh'/Game/InfinityBladeWarriors/Character/CompleteCharacters/SK_CharM_Cardboard.SK_CharM_Cardboard'"));
+
+	if (SK_CARDBOARD.Succeeded())
+	{
+		Mesh->SetSkeletalMesh(SK_CARDBOARD.Object);
+	}
+
+	Mesh->SetAnimationMode(EAnimationMode::AnimationBlueprint);
+
+	static ConstructorHelpers::FClassFinder<UAnimInstance> WARRIOR_ANIM(TEXT("/Script/Engine.AnimBlueprint'/Game/Book/Animations/WarriorAnimBluePrint.WarriorAnimBluePrint_C'"));
+	if (WARRIOR_ANIM.Succeeded())
+	{
+		Mesh->SetAnimInstanceClass(WARRIOR_ANIM.Class);
+	}
 }
 
 // Called when the game starts or when spawned
@@ -40,7 +71,21 @@ void AABPawn::PossessedBy(AController* NewController)
 // Called to bind functionality to input
 void AABPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
+	// InputComponent 에서 사용자의 컨트롤을 위한 입력설정을 Bind 한다.
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis(TEXT("UpDown"), this, &AABPawn::UpDown);
+	PlayerInputComponent->BindAxis(TEXT("LeftRight"), this, &AABPawn::LeftRight);
+}
 
+void AABPawn::UpDown(float NewAxisValue)
+{
+	//AddMovementInput 명령을 사용해 폰을 상하로 이동
+	AddMovementInput(GetActorForwardVector(), NewAxisValue);
+}
+
+void AABPawn::LeftRight(float NewAxisValue)
+{
+	//AddMovementInput 명령을 사용해 폰을 좌우로 이동
+	AddMovementInput(GetActorRightVector(), NewAxisValue);
 }
 
--- Source/ArenaBattle/private/ABPlayerController.cpp
+++ Source/ArenaBattle/private/ABPlayerController.cpp
@@ -3,3 +3,10 @@
 
 #include "ABPlayerController.h"
 
+void AABPlayerController::BeginPlay()
+{
+	// 플레이 시킬때 포커싱하지 않아도 자동으로 구현되게 함.
+	Super::BeginPlay();
+	FInputModeGameOnly InputMode;
+	SetInputMode(InputMode);
+}
