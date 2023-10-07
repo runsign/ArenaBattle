e07c2aa4cb91046ecce8cf6e45fc1177ccc0c1db
runsign <soyoja@naver.com>
2023-10-07 22:38:25 +0900

    094

--- Source/ArenaBattle/Fountain.cpp
+++ Source/ArenaBattle/Fountain.cpp
@@ -24,6 +24,29 @@ AFountain::AFountain()
 	Light->SetRelativeLocation(FVector(0.0f, 0.0f, 195.0f));
 	Splash->SetRelativeLocation(FVector(0.0f, 0.0f, 195.0f));
 
+	// 생성자 코드에서 에셋을 불러오는 기능은 ConstructorHelpers 클래스의 FObjectFinder 를 사용해 변수를 선언한다.
+	ConstructorHelpers::FObjectFinder<UStaticMesh> SM_BODY(TEXT("/Script/Engine.StaticMesh'/Game/InfinityBladeGrassLands/Environments/Plains/Env_Plains_Ruins/StaticMesh/SM_Plains_Castle_Fountain_01.SM_Plains_Castle_Fountain_01'"));
+
+	if (SM_BODY.Succeeded())
+	{
+		Body->SetStaticMesh(SM_BODY.Object);
+	}
+
+	ConstructorHelpers::FObjectFinder<UStaticMesh> SM_WATER(TEXT("/Script/Engine.StaticMesh'/Game/InfinityBladeGrassLands/Effects/FX_Meshes/Env/SM_Plains_Fountain_02.SM_Plains_Fountain_02'"));
+
+	if (SM_WATER.Succeeded())
+	{
+		Water->SetStaticMesh(SM_WATER.Object);
+	}
+
+	ConstructorHelpers::FObjectFinder<UParticleSystem> PS_SPLASH(TEXT("/Script/Engine.ParticleSystem'/Game/InfinityBladeGrassLands/Effects/FX_Ambient/Water/P_Water_Fountain_Splash_Base_01.P_Water_Fountain_Splash_Base_01'"));
+
+	if (SM_BODY.Succeeded())
+	{
+		Splash->SetTemplate(PS_SPLASH.Object);
+	}
+
+
 }
 
 // Called when the game starts or when spawned
