bc188720478fa2f335baaf232b3f4defe3712e1c
runsign <soyoja@naver.com>
2023-10-22 20:56:58 +0900

    367

--- Source/ArenaBattle/public/ABCharacterStatComponent.h
+++ Source/ArenaBattle/public/ABCharacterStatComponent.h
@@ -2,7 +2,7 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
+#include "ArenaBattle.h"
 #include "Components/ActorComponent.h"
 #include "ABCharacterStatComponent.generated.h"
 
@@ -13,16 +13,23 @@ class ARENABATTLE_API UABCharacterStatComponent : public UActorComponent
 	GENERATED_BODY()
 
 public:	
-	// Sets default values for this component's properties
 	UABCharacterStatComponent();
 
 protected:
-	// Called when the game starts
 	virtual void BeginPlay() override;
+	virtual void InitializeComponent() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+public:
+	void SetNewLevel(int32 NewLevel);
 
-		
+private:
+	struct FABCharacterData* CurrentStatData = nullptr;
+	
+	UPROPERTY(EditInstanceOnly, Category = Stat, Meta = (AllowPrivateAccess = true))
+	int32 Level;
+
+	UPROPERTY(Transient, VisibleInstanceOnly, Category = Stat, Meta = (AllowPrivateAccess = true))
+	float CurrentHP;
+
+
 };
